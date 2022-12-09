0826e49373acd3cb36e63881661035d3f55f1dae
MinhThuan1608 <20130426@st.hcmuaf.edu.vn>
2022-12-09 13:09:52 +0700

    commit

--- Scripts/Admin/uploadImage.js
+++ Scripts/Admin/uploadImage.js
@@ -3,23 +3,23 @@ function readURL(input,img,imgInfo) {
 		var reader = new FileReader();
 
 		reader.onload = function(e) {
-			$(img).attr('src', e.target.result);
+			img.attr('src', e.target.result);
 		
 		}
-		$(img).show();
-		$(imgInfo).hide();
+		img.show();
+		imgInfo.hide();
 		reader.readAsDataURL(input.files[0]);
 	}
 }
 
-let imgInp = $('.imgInpOp');
+/*let imgInp = $('.imgInpOp');
 let img = $('.blahOp');
 let imgInfo = $('.imgInfoOp');
 for (let i = 0; i < imgInp.length; i++) {
 	imgInp[i].change(function () {
 		readURL(this, img[i],imgInfo[i]);
     })
-}
+}*/
 
 
 
